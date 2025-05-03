56dc14d96022d74c385ffeb388bd0ea932555f30
CurcoVain <gg.prof.ef@gmail.com>
2025-05-02 22:47:55 -0300

    models terminados

---
+++ models/detalleVenta.py
@@ -0,0 +1,27 @@
+from models.db import db
+
+class DetalleVenta(db.Model):
+    __tablename__= 'detalle_venta'
+    id= db.Column (db.Integer, primary_key=True)
+    venta_id=db.Column(db.Integer, db.ForeignKey('venta.id'), nullable=False)
+    producto_id=db.Column(db.Integer, db.ForeignKey('productos.id'), nullable=False)
+    cantidad= db.Column(db.Integer, nullable=False)
+    precio_unitario=db.Column(db.Float, nullable=False)
+    subtotal=db.Column (db.Float, nullable=False)
+
+    def __init__(self, venta_id, producto_id, cantidad, precio_unitario):
+        self.venta_id = venta_id
+        self.producto_id = producto_id
+        self.cantidad = cantidad
+        self.precio_unitario = precio_unitario
+        self.subtotal = cantidad * precio_unitario  # Lo calculamos automáticamente al crear
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'venta_id': self.venta_id,
+            'producto_id': self.producto_id,
+            'cantidad': self.cantidad,
+            'precio_unitario': self.precio_unitario,
+            'subtotal': self.subtotal
+        }
--- models/venta.py
+++ models/venta.py
@@ -2,13 +2,14 @@ from models.db import db
 from datetime  import datetime
 
 class Venta(db.Model):
-    __tablename__='venta'
+    __tablename__='ventas'
     
     id= db.Column (db.Integer, primary_key=True)
     fecha=db.Column (db.DateTime, default=datetime.now)
     cliente_id=db.Column(db.Integer, db.ForeignKey('clientes.id'), nullable=False)
     descuento=db.Column(db.Float, nullable=False)
     monto_final=db.Column(db.Float, nullable=False)
+    detalles=db.relationship('DetalleVenta', backref='venta', lazy=True)
 
     def __init__(self,  cliente_id, descuento, monto_final):
         self.cliente_id=cliente_id
@@ -21,5 +22,6 @@ class Venta(db.Model):
             'fecha':self.fecha.strftime('%Y%m%d'),
             'cliente_id':self.cliente_id,
             'descuento':self.descuento,
-            'monto_final':self.monto_final
+            'monto_final':self.monto_final,
+            'detalles':[detalle.serialize() for detalle in self.detalles]
         }
